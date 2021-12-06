507d3b50ca9d84fe01962b0a9b17a1c76c3ef385
Iraira <irairarojas@gmail.com>
2021-12-06 08:36:36 -0400

    first draft header

---
+++ src/app/uni-header/uni-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uni-header',
+  templateUrl: './uni-header.component.html',
+  styleUrls: ['./uni-header.component.css']
+})
+export class UniHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
