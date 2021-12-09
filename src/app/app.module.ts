b970d35c18a95b5f92afd611251d388802d76472
SJ2706icecream <SARAH.JIMENEZ@newhorizons.edu.do>
2021-12-09 17:51:14 +0000

    date time picker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UniHeaderComponent } from './uni-header/uni-header.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
 @NgModule({
   declarations: [
     AppComponent,
-    UniHeaderComponent
+    UniHeaderComponent,
+
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     BrowserModule,
     AppRoutingModule
