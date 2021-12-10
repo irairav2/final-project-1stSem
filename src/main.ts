9e20d4763c6aff377fe43ee5fb70e132b303d0a8
irairav2 <IRAIRA.RIVERA@newhorizons.edu.do>
2021-12-09 20:25:17 -0400

    added dt picker to comp, font fam + deets

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { defineCustomElements } from "@duetds/date-picker/dist/loader";
+defineCustomElements(window);
 if (environment.production) {
   enableProdMode();
 }
